31d80814bfa3a7ba8218a38ea42bda56f75d2335
serosc95 <oscardleon95@gmail.com>
2020-12-16 04:35:33 +0000

    Update state with class attributes

--- models/state.py
+++ models/state.py
@@ -1,8 +1,12 @@
 #!/usr/bin/python3
 """ State Module for HBNB project """
 from models.base_model import BaseModel
+from datetime import datetime
+from sqlalchemy import Column, Integer, String, DateTime
+from sqlalchemy.ext.declarative import declarative_base
 
-
-class State(BaseModel):
+class State(BaseModel, Base):
     """ State class """
-    name = ""
+    __tablename__ = "states"
+    name = Column(String(128), nullable=False)
+    addresses = relationship("City", backref="", cascade="all, delete")
