27fa2a68eed5a742deb5d0453f6ae8803a7a5eed
serosc95 <oscardleon95@gmail.com>
2020-12-17 18:07:20 +0000

    PEP8

--- models/engine/db_storage.py
+++ models/engine/db_storage.py
@@ -62,7 +62,7 @@ class DBStorage:
         """Reload objects from the database"""
         Base.metadata.create_all(self.__engine)
         session_factory = sessionmaker(bind=self.__engine,
-                                        expire_on_commit=False)
+                                       expire_on_commit=False)
         Session = scoped_session(session_factory)
         self.__session = Session()
 
