648597f7633d3042c375a34690a2d24912b60418
serosc95 <oscardleon95@gmail.com>
2020-12-18 03:04:25 +0000

    update amenity

--- models/amenity.py
+++ models/amenity.py
@@ -1,11 +1,13 @@
 #!/usr/bin/python3
-""" State Module for HBNB project """
-from models.base_model import BaseModel
+""" Amenity Module for HBNB project """
+from models.base_model import BaseModel, Base
+from sqlalchemy import Column, String, ForeignKey, Float, Integer
+from sqlalchemy.orm import relationship
+from models.place import Place, place_amenity
 
 
-class Amenity(BaseModel):
-    """Comment"""
-    __tablename__ = 'users'
-
-    # Volver columnas
-    name = ""
+class Amenity(BaseModel, Base):
+    """Class attributes"""
+    __tablename__ = 'amenities'
+    name = Column(String(128), nullable=False)
+    place_amenities = relationship('Place', secondary=place_amenity)
